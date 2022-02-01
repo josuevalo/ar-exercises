52fe7a9ed4078a0ccdd07487b8a57ef048328089
Josue <arevalo.josued@gmail.com>
2022-02-01 05:50:37 +0000

    Exercise 5 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -15,4 +15,4 @@ Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, women
 @mens_stores = Store.where(mens_apparel: true)
 @mens_stores.each{|store| puts "name: #{store.name}\n annual revenue: #{store.annual_revenue}"}
 
-@womens_stores = Store.where(womens_apparel: true).where("annual_revenue: <= 1000000")
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue <= 1000000")
